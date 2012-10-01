a7979081f76a4db25a4baaa429e26d38a68fd3dd
Mateusz Parzonka <parzonka@gmail.com>
2012-10-01 19:17:36 +0200

    Add FastIndexingStrategy

---
+++ src/main/java/prm4j/indexing/treebased/impl/FastIndexingStrategy.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.indexing.treebased.impl;
+
+import prm4j.indexing.Event;
+import prm4j.indexing.IndexingStrategy;
+import prm4j.indexing.treebased.BindingStore;
+import prm4j.indexing.treebased.LowLevelBinding;
+
+public class FastIndexingStrategy<A> implements IndexingStrategy<A> {
+
+    private BindingStore<A> bindingStore;
+
+    @Override
+    public void processEvent(Event<A> event) {
+	final LowLevelBinding<A>[] bindings = bindingStore.getBindings(event.getBoundObjects());
+
+	// TODO
+
+    }
+
+}
