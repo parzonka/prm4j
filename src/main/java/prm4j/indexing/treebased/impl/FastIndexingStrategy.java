3a4d6770edfb53c0a26b3618608befaa4a1ac95a
Mateusz Parzonka <parzonka@gmail.com>
2012-10-01 21:30:10 +0200

    Extract method

--- src/main/java/prm4j/indexing/treebased/impl/FastIndexingStrategy.java
+++ src/main/java/prm4j/indexing/treebased/impl/FastIndexingStrategy.java
@@ -37,28 +37,31 @@ public class FastIndexingStrategy<A> implements IndexingStrategy<A> {
 	if (instanceMonitor == null) {
 	    // TODO join and chain with implicit update
 	    for (JoinData joinData : eventContext.getJoinData(event)) {
-		final Node<A> compatibleNode = nodeStore.getNode(bindings, joinData.getTraversalMask());
-		// calculate once the bindings to be joined with the whole monitor set
-		final LowLevelBinding<A>[] joinableBindings = getJoinableBindings(bindings,
-			joinData.getJoinTargetPattern());
-
-		for (int parameterSetId : joinData.getParameterSetIds()) {
-		    // iterate over all compatible nodes
-		    MonitorSetIterator<A> iter = compatibleNode.getMonitorSet(parameterSetId).getIterator();
-		    AbstractBaseMonitor<A> compatibleMonitor = null;
-		    boolean isCompatibleMonitorAlive = false;
-		    while (iter.hasNext(compatibleMonitor, isCompatibleMonitorAlive)) {
-			compatibleMonitor = iter.next();
-			isCompatibleMonitorAlive = expand(compatibleMonitor, joinableBindings,
-				joinData.getJoinSourcePattern(), joinData.getDisableMask(), event);
-		    }
-		}
+		joinWithAllCompatibleInstances(joinData, bindings, event);
 	    }
 
 	} else {
 	    // TODO just update
 	}
 
+    }
+
+    private void joinWithAllCompatibleInstances(final JoinData joinData, final LowLevelBinding<A>[] bindings, final Event<A> event) {
+
+	final Node<A> compatibleNode = nodeStore.getNode(bindings, joinData.getTraversalMask());
+	// calculate once the bindings to be joined with the whole monitor set
+	final LowLevelBinding<A>[] joinableBindings = getJoinableBindings(bindings, joinData.getJoinTargetPattern());
+	for (int parameterSetId : joinData.getParameterSetIds()) {
+	    // iterate over all compatible nodes
+	    final MonitorSetIterator<A> iter = compatibleNode.getMonitorSet(parameterSetId).getIterator();
+	    AbstractBaseMonitor<A> compatibleMonitor = null;
+	    boolean isCompatibleMonitorAlive = false;
+	    while (iter.hasNext(compatibleMonitor, isCompatibleMonitorAlive)) {
+		compatibleMonitor = iter.next();
+		isCompatibleMonitorAlive = expand(compatibleMonitor, joinableBindings, joinData.getJoinSourcePattern(),
+			joinData.getDisableMask(), event);
+	    }
+	}
     }
 
     /**
