f9109efd686ef2c6aa222d8cd6f29e47110b0d6d
Mateusz Parzonka <parzonka@gmail.com>
2012-10-02 13:44:57 +0200

    Add FSMSpec

---
+++ src/main/java/prm4j/api/fsm/FSMSpec.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.api.fsm;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import prm4j.api.Symbol;
+import prm4j.logic.MonitorState;
+import prm4j.logic.StatefulSpec;
+
+public class FSMSpec implements StatefulSpec {
+
+    @Override
+    public List<Symbol> getSymbols() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public Map<Symbol, Set<Set<Symbol>>> getPropertyEnableSet() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public Map<MonitorState<?>, Set<Set<Symbol>>> getStateCoEnableSet() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public MonitorState<?> getInitialState() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
