27e2c23122ab8352adac5dbfcd5657b53732a38c
Mateusz Parzonka <parzonka@gmail.com>
2012-10-02 18:31:49 +0200

    Test method

--- src/test/java/prm4j/logic/StatefulSpecProcessorTest.java
+++ src/test/java/prm4j/logic/StatefulSpecProcessorTest.java
@@ -10,10 +10,18 @@
  */
 package prm4j.logic;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
-import static junit.framework.Assert.*;
 
 import prm4j.AbstractTest;
+import prm4j.api.Symbol;
 import prm4j.api.fsm.FSM;
 import prm4j.api.fsm.FSMSpec;
 
@@ -26,6 +34,30 @@ public class StatefulSpecProcessorTest extends AbstractTest {
 
 	assertEquals(ssp.getInitialState(), fsm.getInitialState());
 	assertEquals(ssp.getSymbols(), fsm.getAlphabet().getSymbols());
+    }
+
+    @Test
+    public void getPropertyEnableSets_unsafeMapIterator() throws Exception {
+	FSM_unsafeMapIterator u = new FSM_unsafeMapIterator();
+	FSM<Void> fsm = u.fsm;
+	StatefulSpecProcessor ssp = new StatefulSpecProcessor(new FSMSpec<Void>(fsm));
+
+	Map<Symbol, Set<Set<Symbol>>> actual = ssp.getPropertyEnableSets();
+
+	Map<Symbol, Set<Set<Symbol>>> expected = new HashMap<Symbol, Set<Set<Symbol>>>();
+	for (Symbol symbol : u.alphabet.getSymbols()) {
+	    expected.put(symbol, new HashSet<Set<Symbol>>());
+	}
+	expected.get(u.createColl).add(Collections.<Symbol> emptySet());
+	expected.get(u.createIter).add(asSet(u.createColl));
+	expected.get(u.createIter).add(asSet(u.createColl, u.updateMap));
+	expected.get(u.useIter).add(asSet(u.createColl, u.createIter));
+	expected.get(u.useIter).add(asSet(u.createColl, u.createIter, u.updateMap));
+	expected.get(u.updateMap).add(asSet(u.createColl));
+	expected.get(u.updateMap).add(asSet(u.createColl, u.createIter));
+	expected.get(u.updateMap).add(asSet(u.createColl, u.createIter, u.useIter));
+
+	assertEquals(expected, actual);
     }
 
 }
