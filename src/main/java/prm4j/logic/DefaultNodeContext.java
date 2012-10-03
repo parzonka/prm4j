db700327b60731f6bc21feb92553e83c819d2991
Mateusz Parzonka <parzonka@gmail.com>
2012-10-03 13:41:42 +0200

    Add DefaultNodeContext

---
+++ src/main/java/prm4j/logic/DefaultNodeContext.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.logic;
+
+import prm4j.indexing.treebased.MonitorSet;
+import prm4j.indexing.treebased.Node;
+import prm4j.indexing.treebased.NodeMap;
+
+public class DefaultNodeContext<A> implements NodeContext<A>{
+
+    @Override
+    public ChainingData[] getChainingData() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public MonitorSet<A> createMonitorSet() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public Node<A> createNode(int parameterId) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public NodeMap<A> createNodeMap() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
