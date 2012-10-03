bb56a0c3d639790564e7b8d8a0e91748dad3ae00
Mateusz Parzonka <parzonka@gmail.com>
2012-10-04 01:07:29 +0200

    Add Symbol0 for parameterless events

---
+++ src/main/java/prm4j/api/Symbol0.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.api;
+
+import prm4j.indexing.Event;
+
+/**
+ * Parameterless symbol.
+ */
+public class Symbol0 extends Symbol {
+
+    Symbol0(Alphabet alphabet, int uniqueId, String uniqueName) {
+	super(alphabet, uniqueId, uniqueName, 0);
+    }
+
+    public Event<Void> createEvent() {
+	Object[] boundObjects = createObjectArray();
+	return new Event<Void>(this, boundObjects);
+    }
+
+    public <A> Event<A> createEventWithAuxiliaryData(A auxiliaryData) {
+	Object[] boundObjects = createObjectArray();
+	return new Event<A>(this, auxiliaryData, boundObjects);
+    }
+
+}
