477fda5dfa1ebfd2bb5f6cce0320d8f0ef3868e0
Mateusz Parzonka <parzonka@gmail.com>
2012-10-11 02:21:46 +0200

    Fix typo

--- src/main/java/prm4j/logic/StatefulMonitor.java
+++ src/main/java/prm4j/logic/StatefulMonitor.java
@@ -33,7 +33,7 @@ public class StatefulMonitor extends AbstractBaseMonitor<Symbol> {
 	MatchHandler matchHandler = state.getMatchHandler();
 	if (matchHandler != null) {
 	    matchHandler.handleMatch(getBindings());
-	    // when a state is a final state it is still possible we will reach another final state (or loop on the a
+	    // when a state is a final state, it is still possible we will reach another final state (or loop on a
 	    // final state), so we don't return false here
 	}
 	return true;
