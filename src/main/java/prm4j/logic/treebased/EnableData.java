ea45278c96b541856c6c81a382739be40dbb486d
Mateusz Parzonka <parzonka@gmail.com>
2012-10-29 13:49:57 +0100

    Add EnableData

---
+++ src/main/java/prm4j/logic/treebased/EnableData.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.logic.treebased;
+
+public class EnableData {
+
+    private final int[] nodeMask;
+    private final int[] diffMask;
+
+    public EnableData(int[] nodeMask, int[] diffMask) {
+	super();
+	this.nodeMask = nodeMask;
+	this.diffMask = diffMask;
+    }
+
+    public int[] getNodeMask() {
+	return nodeMask;
+    }
+
+    public int[] getDiffMask() {
+	return diffMask;
+    }
+
+}
