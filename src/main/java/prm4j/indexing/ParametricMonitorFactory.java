08dc4d241c4a47e0562b01a2f7807beb5944dfbb
Mateusz Parzonka <parzonka@gmail.com>
2012-10-30 01:51:02 +0100

    Start ParametricMonitorFactory

---
+++ src/main/java/prm4j/indexing/ParametricMonitorFactory.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.indexing;
+
+import prm4j.logic.FiniteSpec;
+import prm4j.logic.ParametricPropertyImpl;
+import prm4j.logic.treebased.LowLevelParametricProperty;
+
+public class ParametricMonitorFactory {
+
+    public <M extends BaseMonitor<M>> ParametricMonitor<M> createParametricMonitor(FiniteSpec finiteSpec, Class<M> baseMonitorType) {
+	LowLevelParametricProperty pp = new LowLevelParametricProperty(new ParametricPropertyImpl(finiteSpec));
+
+	return null;
+    }
+
+}
