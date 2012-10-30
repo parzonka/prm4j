67c112f4ef5ccc169902b6d0a79568952be548c7
Mateusz Parzonka <parzonka@gmail.com>
2012-10-30 11:14:00 +0100

    Fix doubled header

--- src/main/java/prm4j/indexing/treebased/Node.java
+++ src/main/java/prm4j/indexing/treebased/Node.java
@@ -10,16 +10,6 @@
  */
 package prm4j.indexing.treebased;
 
-/*
- * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * Mateusz Parzonka - initial API and implementation
- */
 import prm4j.indexing.AbstractBaseMonitor;
 import prm4j.logic.treebased.MetaNode;
 
