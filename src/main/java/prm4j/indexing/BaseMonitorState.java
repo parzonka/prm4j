6a46ea7a33a97ffbba7a51bbb41fe9bffae38c6b
Mateusz Parzonka <parzonka@gmail.com>
2012-11-05 14:50:41 +0100

    Add reachable accepting state check in BaseMonitorState

--- src/main/java/prm4j/indexing/BaseMonitorState.java
+++ src/main/java/prm4j/indexing/BaseMonitorState.java
@@ -12,13 +12,37 @@ package prm4j.indexing;
 
 import prm4j.api.BaseEvent;
 import prm4j.api.MatchHandler;
+import prm4j.indexing.realtime.LowLevelBinding;
 
 public abstract class BaseMonitorState {
+
+    private boolean[] acceptingParameters;
+
+    /**
+     * Tests, if a accepting state can be reached by some event in the future. Checks interally, if all bindings are
+     * still alive, needed to reach a accepting state.
+     *
+     * @param bindings bindings to be checked
+     * @return true, if accepting state can be reached
+     */
+    public boolean isFinalStateReachable(LowLevelBinding[] bindings) {
+	for (LowLevelBinding b : bindings) {
+	    if (acceptingParameters[b.getParameterId()]) {
+		if (b.get() == null)
+		    return false;
+	    }
+	}
+	return true;
+    }
 
     public abstract BaseMonitorState getSuccessor(BaseEvent baseEvent);
 
     public abstract boolean isFinal();
 
     public abstract MatchHandler getMatchHandler();
+
+    public void setAcceptingParameters(boolean[] acceptingParameters) {
+	this.acceptingParameters = acceptingParameters;
+    }
 
 }
