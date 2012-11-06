8a39bcbd91848b8ef541390987ee2be398a789a8
Mateusz Parzonka <parzonka@gmail.com>
2012-11-06 20:18:15 +0100

    Fix warning

--- src/main/java/prm4j/indexing/staticdata/StaticDataConverter.java
+++ src/main/java/prm4j/indexing/staticdata/StaticDataConverter.java
@@ -17,7 +17,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import prm4j.Util;
