f216c1ba62ee66326dc25c07f3b948cbb5a4eea6
Mateusz Parzonka <parzonka@gmail.com>
2012-11-06 04:02:12 +0100

    Add hashCode and equals for Tuple

--- src/main/java/prm4j/Util.java
+++ src/main/java/prm4j/Util.java
@@ -97,6 +97,38 @@ public class Util {
 	    return right;
 	}
 
+	@Override
+	public int hashCode() {
+	    final int prime = 31;
+	    int result = 1;
+	    result = prime * result + ((left == null) ? 0 : left.hashCode());
+	    result = prime * result + ((right == null) ? 0 : right.hashCode());
+	    return result;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public boolean equals(Object obj) {
+	    if (this == obj)
+		return true;
+	    if (obj == null)
+		return false;
+	    if (getClass() != obj.getClass())
+		return false;
+	    Tuple other = (Tuple) obj;
+	    if (left == null) {
+		if (other.left != null)
+		    return false;
+	    } else if (!left.equals(other.left))
+		return false;
+	    if (right == null) {
+		if (other.right != null)
+		    return false;
+	    } else if (!right.equals(other.right))
+		return false;
+	    return true;
+	}
+
     }
 
 }
