70ede100b13c55e43438be44cd05daa94db2d97c
Mateusz Parzonka <parzonka@gmail.com>
2012-11-08 00:30:33 +0100

    Add hashCode, equals and toString to ChainData

--- src/main/java/prm4j/indexing/staticdata/ChainData.java
+++ src/main/java/prm4j/indexing/staticdata/ChainData.java
@@ -10,6 +10,8 @@
  */
 package prm4j.indexing.staticdata;
 
+import java.util.Arrays;
+
 /**
  * Represents all less informative instances of the event instance which match its enable sets and are associated with
  * symbols, PLUS the compatible partial instances which map to parameter sets to fully instantiate joinable instances.
@@ -31,6 +33,36 @@ public class ChainData {
 
     public int getMonitorSetId() {
 	return monitorSetId;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + monitorSetId;
+	result = prime * result + Arrays.hashCode(nodeMask);
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	ChainData other = (ChainData) obj;
+	if (monitorSetId != other.monitorSetId)
+	    return false;
+	if (!Arrays.equals(nodeMask, other.nodeMask))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "ChainData [nodeMask=" + Arrays.toString(nodeMask) + ", monitorSetId=" + monitorSetId + "]";
     }
 
 }
