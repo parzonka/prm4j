c6dfe3151cf9104306b90d18dfc0f232da3a7e11
Mateusz Parzonka <parzonka@gmail.com>
2012-11-12 21:16:59 +0100

    Add initial tests for MinimalMap

---
+++ src/test/java/prm4j/indexing/map/MinimalMapTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.indexing.map;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MinimalMapTest {
+
+    @Test
+    public void size_1() throws Exception {
+	MinimalMap<MockEntry> map = new MockMap();
+	assertEquals(0, map.size());
+	String a = "a";
+	map.get(a);
+	assertEquals(1, map.size());
+	map.get(a);
+	assertEquals(1, map.size());
+	map.get("b");
+	assertEquals(2, map.size());
+    }
+
+    @Test
+    public void get_1() throws Exception {
+	MinimalMap<MockEntry> map = new MockMap();
+	String a = "a";
+	MockEntry x = map.get(a);
+	MockEntry y = map.get(a);
+	assertEquals(x, y);
+    }
+
+    static class MockMap extends MinimalMap<MockEntry> {
+
+	@Override
+	protected MockEntry[] createInitialArray(int size) {
+	    return new MockEntry[size];
+	}
+
+	@Override
+	protected MockEntry createEntry(Object key, int hashCode) {
+	    return new MockEntry(key, hashCode);
+	}
+
+    }
+
+    static class MockEntry implements MinimalMapEntry<MockEntry> {
+
+	private final Object key;
+	private final int hashCode;
+	private MockEntry next;
+
+	public MockEntry(Object key, int hashCode) {
+	    this.key = key;
+	    this.hashCode = hashCode;
+	}
+
+	@Override
+	public int getHashCode() {
+	    return hashCode;
+	}
+
+	@Override
+	public Object getKey() {
+	    return key;
+	}
+
+	@Override
+	public MockEntry next() {
+	    return next;
+	}
+
+	@Override
+	public void setNext(MockEntry nextEntry) {
+	    next = nextEntry;
+	}
+
+    }
+
+}
