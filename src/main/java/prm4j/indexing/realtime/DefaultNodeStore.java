be7caae75952c468c1d47ed696542d3465b10b33
Mateusz Parzonka <parzonka@gmail.com>
2012-11-13 19:11:07 +0100

    Improve DefaultNodeStore

--- src/main/java/prm4j/indexing/realtime/DefaultNodeStore.java
+++ src/main/java/prm4j/indexing/realtime/DefaultNodeStore.java
@@ -14,22 +14,17 @@ import prm4j.indexing.staticdata.MetaNode;
 
 public class DefaultNodeStore implements NodeStore {
 
-    private Node rootNode;
+    private final Node rootNode;
 
     public DefaultNodeStore(MetaNode metaTree) {
-	 rootNode = metaTree.createRootNode();
+	rootNode = metaTree.createRootNode();
     }
 
     @Override
     public Node getNode(LowLevelBinding[] bindings) {
-	// fast track for the parameterless instance
-	if (bindings.length == 0) {
-	    return rootNode;
-	}
-	// retrieve the head of the array pointing to the first node we will use
-	Node node = rootNode.getNode(bindings[0]);
+	Node node = rootNode;
 	// we iterate over the rest { node1 , ..., nodeN }, traversing the tree
-	for (int i = 1; i < bindings.length; i++) {
+	for (int i = 0; i < bindings.length; i++) {
 	    // traverse the node tree until the parameter instance is fully realized
 	    node = node.getNode(bindings[i]);
 	}
@@ -38,14 +33,9 @@ public class DefaultNodeStore implements NodeStore {
 
     @Override
     public Node getNode(LowLevelBinding[] bindings, int[] parameterMask) {
-	// fast track for the parameterless instance
-	if (parameterMask.length == 0) {
-	    return rootNode;
-	}
-	// retrieve the head of the array pointing to the first node we will use
-	Node node = rootNode.getNode(bindings[parameterMask[0]]);
+	Node node = rootNode;
 	// we iterate over the rest { node1 , ..., nodeN }, traversing the tree
-	for (int i = 1; i < parameterMask.length; i++) {
+	for (int i = 0; i < parameterMask.length; i++) {
 	    // traverse the node tree until the parameter instance is fully realized
 	    node = node.getNode(bindings[parameterMask[i]]);
 	}
