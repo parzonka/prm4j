21d175a284981fc4587db4c2839373b57ca3459e
Mateusz Parzonka <parzonka@gmail.com>
2012-11-14 23:00:08 +0100

    Add creation of binary AwareMatchHandlers

--- src/test/java/prm4j/indexing/realtime/AwareMatchHandler.java
+++ src/test/java/prm4j/indexing/realtime/AwareMatchHandler.java
@@ -13,13 +13,20 @@ package prm4j.indexing.realtime;
 import java.util.ArrayList;
 import java.util.List;
 
+import prm4j.Util;
+import prm4j.Util.Tuple;
 import prm4j.api.MatchHandler1;
+import prm4j.api.MatchHandler2;
 import prm4j.api.Parameter;
 
 public class AwareMatchHandler<P1> {
 
     public static <P1> AwareMatchHandler1<P1> create(Parameter<P1> param1) {
 	return new AwareMatchHandler1<P1>(param1);
+    }
+
+    public static <P1, P2> AwareMatchHandler2<P1, P2> create(Parameter<P1> param1, Parameter<P2> param2) {
+	return new AwareMatchHandler2<P1, P2>(param1, param2);
     }
 
     public static class AwareMatchHandler1<P1> extends MatchHandler1<P1> {
@@ -37,11 +44,36 @@ public class AwareMatchHandler<P1> {
 	}
 
 	/**
-	 * Returns a list of handled matches for the given object.
+	 * Returns a list of object which participated in a detected match.
 	 *
 	 * @return
 	 */
 	public List<P1> getHandledMatches() {
+	    return handledMatches;
+	}
+
+    }
+
+    public static class AwareMatchHandler2<P1, P2> extends MatchHandler2<P1, P2> {
+
+	private final List<Tuple<P1, P2>> handledMatches;
+
+	public AwareMatchHandler2(Parameter<P1> param1, Parameter<P2> param2) {
+	    super(param1, param2);
+	    handledMatches = new ArrayList<Tuple<P1, P2>>();
+	}
+
+	@Override
+	public void handleMatch(P1 obj1, P2 obj2) {
+	    handledMatches.add(Util.tuple(obj1, obj2));
+	}
+
+	/**
+	 * Returns a list of object tuples which participated in a detected match.
+	 *
+	 * @return
+	 */
+	public List<Tuple<P1, P2>> getHandledMatches() {
 	    return handledMatches;
 	}
 
