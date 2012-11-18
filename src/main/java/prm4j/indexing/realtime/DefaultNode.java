018f8b9bf9436b550c1158d2ee2f55a4ae8e50ab
Mateusz Parzonka <parzonka@gmail.com>
2012-11-18 19:23:34 +0100

    Improve toString method for non-monitor nodes

--- src/main/java/prm4j/indexing/realtime/DefaultNode.java
+++ src/main/java/prm4j/indexing/realtime/DefaultNode.java
@@ -11,6 +11,7 @@
 package prm4j.indexing.realtime;
 
 import prm4j.Util;
+import prm4j.api.Parameter;
 import prm4j.indexing.BaseMonitor;
 import prm4j.indexing.staticdata.MetaNode;
 
@@ -76,7 +77,25 @@ public class DefaultNode extends AbstractNode {
 
     @Override
     public String toString() {
-	return monitor == null ? "(..., " + getKey() + ")" : Util.bindingsToString(monitor.getLowLevelBindings());
+	if (monitor != null) {
+	    // output e.g.: (p2=b, p3=c, p5=e)
+	    return Util.bindingsToString(monitor.getLowLevelBindings());
+	} else {
+	    // output e.g.: (p2=?, p3=?, p5=e) because we only now the key and the node parameter set
+	    final StringBuilder sb = new StringBuilder();
+	    sb.append("(");
+	    int i = 0;
+	    final int max = metaNode.getNodeParameterList().size();
+	    for (Parameter<?> parameter : metaNode.getNodeParameterList()) {
+		if (++i < max) {
+		    sb.append(parameter).append("=?, ");
+		} else {
+		    sb.append(getKey()).append(")");
+		    break;
+		}
+	    }
+	    return sb.toString();
+	}
     }
 
 }
