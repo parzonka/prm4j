d67ca44a782c1851f26fdc1c993a9f5fa9e23b3e
Mateusz Parzonka <parzonka@gmail.com>
2012-12-12 15:19:10 +0100

    Fix import

--- src/test/java/prm4j/spec/FiniteParametricPropertySafeSyncCollectionTest.java
+++ src/test/java/prm4j/spec/FiniteParametricPropertySafeSyncCollectionTest.java
@@ -13,7 +13,6 @@ package prm4j.spec;
 import static org.junit.Assert.assertEquals;
 import static prm4j.Util.tuple;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
