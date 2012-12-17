220970ada064598822bed9817953ddf635c335dd
Mateusz Parzonka <parzonka@gmail.com>
2012-12-17 08:50:30 +0100

    Add detailed comments for NodeManager

--- src/main/java/prm4j/indexing/realtime/NodeManager.java
+++ src/main/java/prm4j/indexing/realtime/NodeManager.java
@@ -12,45 +12,111 @@ package prm4j.indexing.realtime;
 
 import java.lang.ref.ReferenceQueue;
 
+import prm4j.api.ParametricMonitor;
+import prm4j.indexing.staticdata.NodeFactory;
+
+/**
+ * Coordinates the garbage collection of monitors which can never reach an accepting state. Provides some diagnostics
+ * regarding node creation.
+ */
 public class NodeManager {
 
-    private long createdCount;
-    private long cleanedCount;
+    /**
+     * Interval for polling the reference queue for {@link NodeRef}s after garbage-collection of {@link Node}s. The
+     * interval is measured in number of events processed by the {@link ParametricMonitor} (aka 'timestamp'). Larger
+     * numbers (e.g. 100000) have proven sufficient, since {@link NodeRef}s containing the orphaned monitors usually get
+     * garbage collected themselves quite quickly.
+     */
+    private final static int CLEANING_INTERVAL = 100000;
 
+    /**
+     * The number of created nodes by each {@link NodeFactory}.
+     */
+    private long createdNodeCount;
+
+    /**
+     * The number of monitors for which their associated {@link Node} has been garbage collected.
+     */
+    private long orphanedMonitors;
+
+    /**
+     * The number of orphaned monitors that could never reach an accepting state and got garbage collected.
+     */
+    private long collectedMonitors;
+
+    /**
+     * Contains {@link NodeRef}s.
+     */
     private final ReferenceQueue<Node> referenceQueue;
 
     public NodeManager() {
 	referenceQueue = new ReferenceQueue<Node>();
     }
 
-    public ReferenceQueue<Node> getReferenceQueue() {
-	return referenceQueue;
-    }
-
+    /**
+     * Calls {@link #reallyClean()} each time the cleaning interval is reached.
+     *
+     * @param timestamp
+     */
     public void tryToClean(long timestamp) {
-	if (timestamp % 100000 == 0)
+	if (timestamp % CLEANING_INTERVAL == 0)
 	    reallyClean();
     }
 
+    /**
+     * Polls all expired {@link NodeRef}s and nullifies all monitors which can never reach an accepting state.
+     */
     public void reallyClean() {
 	NodeRef nodeRef = (NodeRef) referenceQueue.poll();
 	while (nodeRef != null) {
-	    cleanedCount++;
-	    // TODO clean monitor
+	    orphanedMonitors++;
+	    if (!nodeRef.monitor.isAcceptingStateReachable()) {
+		nodeRef.monitor = null;
+		collectedMonitors++;
+	    }
 	    nodeRef = (NodeRef) referenceQueue.poll();
 	}
     }
 
-    public Object getCleanedCount() {
-	return cleanedCount;
-    }
-
+    /**
+     * DIAGNOSTIC: Called by each {@link NodeFactory} each time a node has been created.
+     *
+     * @param node
+     */
     public void createdNode(Node node) {
-	createdCount++;
+	createdNodeCount++;
     }
 
+    /**
+     * DIAGNOSTIC: Returns the number of created nodes by each {@link NodeFactory}.
+     *
+     * @return the number of created nodes
+     */
     public long getCreatedCount() {
-	return createdCount;
+	return createdNodeCount;
+    }
+
+    public ReferenceQueue<Node> getReferenceQueue() {
+	return referenceQueue;
+    }
+
+    /**
+     * DIAGNOSTIC: Returns the number of monitors for which their associated {@link Node} has been garbage collected.
+     *
+     * @return the number of orphaned monitors
+     */
+    public long getOrphanedMonitorsCount() {
+	return orphanedMonitors;
+    }
+
+    /**
+     * DIAGNOSTIC: Returns the number of orphaned monitors that could never reach an accepting state and got garbage
+     * collected.
+     *
+     * @return the number of garbage collected monitors
+     */
+    public long getCollectedMonitors() {
+	return collectedMonitors;
     }
 
 }
