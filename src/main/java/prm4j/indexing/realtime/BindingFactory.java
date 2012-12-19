63122988dbf2bb3aa92510299aa9c41337439a06
Mateusz Parzonka <parzonka@gmail.com>
2012-12-19 14:56:17 +0100

    Add BindingFactory with default implementation and UnaryBindingFactory

---
+++ src/main/java/prm4j/indexing/realtime/BindingFactory.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.indexing.realtime;
+
+import java.lang.ref.ReferenceQueue;
+
+public interface BindingFactory {
+
+    public LowLevelBinding[] createTable(int size);
+
+    public LowLevelBinding createBinding(Object boundObject, int hashCode, ReferenceQueue<Object> referenceQueue,
+	    int fullParameterCount);
+
+}
---
+++ src/main/java/prm4j/indexing/realtime/DefaultBindingFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.indexing.realtime;
+
+import java.lang.ref.ReferenceQueue;
+
+public class DefaultBindingFactory implements BindingFactory {
+
+    @Override
+    public LowLevelBinding[] createTable(int size) {
+	return new DefaultLowLevelBinding[size];
+    }
+
+    @Override
+    public LowLevelBinding createBinding(Object boundObject, int hashCode, ReferenceQueue<Object> referenceQueue,
+	    int fullParameterCount) {
+	return new DefaultLowLevelBinding(boundObject, hashCode, referenceQueue, fullParameterCount);
+    }
+
+}
