7e6a40e36fef36a92ffcd09c3ec6eaa7b484d0ae
Mateusz Parzonka <parzonka@gmail.com>
2013-01-25 23:39:48 +0100

    Test FiniteParametricProperty based on HasNext

---
+++ src/test/java/prm4j/spec/FiniteParametricPropertyHasNextTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j.spec;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+
+import prm4j.AbstractTest;
+import prm4j.api.Parameter;
+import prm4j.api.fsm.FSMSpec;
+import prm4j.indexing.BaseMonitorState;
+import prm4j.indexing.realtime.NodeManager;
+import prm4j.indexing.staticdata.StaticDataConverter;
+
+public class FiniteParametricPropertyHasNextTest extends AbstractTest {
+
+    FSM_HasNext fsm;
+    FiniteParametricProperty fpp;
+
+    @Before
+    public void init() {
+	fsm = new FSM_HasNext();
+	fpp = new FiniteParametricProperty(new FSMSpec(fsm.fsm));
+    }
+
+    @Test
+    public void getCreationEvents() throws Exception {
+	StaticDataConverter sdc = new StaticDataConverter(fpp);
+	sdc.getMetaTree().setNodeManagerToTree(new NodeManager());
+    }
+
+    @Test
+    public void getAliveParameterSets() throws Exception {
+	SetMultimap<BaseMonitorState, Set<Parameter<?>>> actual = fpp.getAliveParameterSets();
+	// verify
+	SetMultimap<BaseMonitorState, Set<Parameter<?>>> expected = HashMultimap.create();
+
+	expected.put(fsm.initial, asSet(fsm.i));
+	expected.put(fsm.safe, asSet(fsm.i));
+	expected.put(fsm.error, asSet(fsm.i));
+
+	assertEquals(expected, actual);
+    }
+
+}
