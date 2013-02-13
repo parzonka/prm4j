be1e55a84aaf2addff1e298fe73851fe87509250
Mateusz Parzonka <parzonka@gmail.com>
2013-02-13 19:25:07 +0100

    Update copyright header according to EPL notice standard

--- src/main/java/prm4j/api/ParametricMonitor.java
+++ src/main/java/prm4j/api/ParametricMonitor.java
@@ -1,13 +1,12 @@
 /*
- * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * Copyright (c) 2012, 2013 Mateusz Parzonka
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * Eric Bodden - initial API and implementation
- * Mateusz Parzonka - adapted API and implementation
+ * Mateusz Parzonka - initial API and implementation
  */
 package prm4j.api;
 
